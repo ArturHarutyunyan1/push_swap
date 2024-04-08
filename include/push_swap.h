6c41f3f111b6a607fdba4cee789ce5b5499005fd
Artur Harutyunyan <arturhar@c1r2s4.42yerevan.am>
2024-04-08 11:47:21 +0400

    Norminette erros

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arturhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:26:31 by arturhar          #+#    #+#             */
-/*   Updated: 2024/04/07 12:26:32 by arturhar         ###   ########.fr       */
+/*   Updated: 2024/04/08 11:45:57 by arturhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -22,51 +22,51 @@
 
 typedef struct s_stack
 {
-	int	num;
-	int	index;
+	int				num;
+	int				index;
 	struct s_stack	*next;
-}	t_stack;
+}				t_stack;
 
-void	sa(t_stack **stack);
-void	sb(t_stack **stack);
-void	ss(t_stack **stack_a, t_stack **stack_b);
-void	pa(t_stack **stack_a, t_stack **stack_b);
-void	pb(t_stack **stack_a, t_stack **stack_b);
-void	ra(t_stack **stack_a);
-void	rb(t_stack **stack);
-void	rr(t_stack **stack_a, t_stack **stack_b);
-void	rra(t_stack **stack);
-void	rra(t_stack **stack);
-void	rrb(t_stack **stack);
-void	rrr(t_stack **stack_a, t_stack **stack_b);
-void	check(t_stack **stack);
-void	push_to_stack(t_stack **stack, int num);
-void	set_index(t_stack **stack);
-void	add_to_stack(t_stack **stack, int argc, char **argv);
-void	free_stack(t_stack **stack);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	free_split(char **str);
-void	sort_2(t_stack **stack);
-void	sort_3(t_stack **stack);
-void	sort_5(t_stack **stack_a, t_stack **stack_b, int size);
-void	sort(t_stack **stack_a, t_stack **stack_b, int size);
-void	push_back(t_stack **stack_a, t_stack **stack_b, int size);
-void	butterfly(t_stack **stack_a, t_stack **stack_b, int size);
-void	print_error(void);
+void		sa(t_stack **stack);
+void		sb(t_stack **stack);
+void		ss(t_stack **stack_a, t_stack **stack_b);
+void		pa(t_stack **stack_a, t_stack **stack_b);
+void		pb(t_stack **stack_a, t_stack **stack_b);
+void		ra(t_stack **stack_a);
+void		rb(t_stack **stack);
+void		rr(t_stack **stack_a, t_stack **stack_b);
+void		rra(t_stack **stack);
+void		rra(t_stack **stack);
+void		rrb(t_stack **stack);
+void		rrr(t_stack **stack_a, t_stack **stack_b);
+void		check(t_stack **stack);
+void		push_to_stack(t_stack **stack, int num);
+void		set_index(t_stack **stack);
+void		add_to_stack(t_stack **stack, int argc, char **argv);
+void		free_stack(t_stack **stack);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		free_split(char **str);
+void		sort_2(t_stack **stack);
+void		sort_3(t_stack **stack);
+void		sort_5(t_stack **stack_a, t_stack **stack_b, int size);
+void		sort(t_stack **stack_a, t_stack **stack_b, int size);
+void		push_back(t_stack **stack_a, t_stack **stack_b, int size);
+void		butterfly(t_stack **stack_a, t_stack **stack_b, int size);
+void		print_error(void);
 
 long long	ft_atoi(const char *str);
-int		ft_isdigit(int c);
-int		is_sorted(t_stack *head);
-int		ft_sqrt(int nb);
-int		get_pos(t_stack *stack, int i);
-int		stack_length(t_stack **stack);
+int			ft_isdigit(int c);
+int			is_sorted(t_stack *head);
+int			ft_sqrt(int nb);
+int			get_pos(t_stack *stack, int i);
+int			stack_length(t_stack **stack);
 
-bool	validator(int argc, char **argv);
-bool	is_all_num(char **str);
+bool		validator(int argc, char **argv);
+bool		is_all_num(char **str);
 
-char	**ft_split(char const *s, char c);
-char	*ft_strchr(const char *str, int c);
+char		**ft_split(char const *s, char c);
+char		*ft_strchr(const char *str, int c);
 
-size_t	ft_strlen(const char *str);
-t_stack	*ft_lstnew(int num);
+size_t		ft_strlen(const char *str);
+t_stack		*ft_lstnew(int num);
 #endif
